2f97a092ce2215293328b4801eb8ca9afe112945
Pedro Luiz Ferreira <161030385+PedroLuizFerreira@users.noreply.github.com>
2024-03-01 10:35:25 -0300

    code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const texto = document.body.querySelector("#texto");
     texto.addEventListener("click", () => {
         if (texto.textContent==="Olá Mundo")
-        texto.textContent="Hello Wolrd";
+        texto.textContent="Hello World";
         else
         texto.textContent="Olá Mundo"
     });
