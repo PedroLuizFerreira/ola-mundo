6ce312a5dc2f2c46053da118460ab0f38b77413e
Pedro Luiz Ferreira <161030385+PedroLuizFerreira@users.noreply.github.com>
2024-03-01 10:37:34 -0300

    Delete code.js

--- code.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const texto = document.body.querySelector("#texto");
-    texto.addEventListener("click", () => {
-        if (texto.textContent==="Olá Mundo")
-        texto.textContent="Hello World";
-        else
-        texto.textContent="Olá Mundo"
-    });
-});
